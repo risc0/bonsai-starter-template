5bef2fba66d3792661122f76d1bca2ad865e9d51
Victor Graf <victor@subgraf.dev>
2023-02-24 13:54:25 -0800

    adjust guest input encoding

--- methods/guest/src/bin/fibonacci.rs
+++ methods/guest/src/bin/fibonacci.rs
@@ -15,10 +15,6 @@
 #![no_main]
 #![no_std]
 
-extern crate alloc;
-
-use alloc::vec::Vec;
-
 use ethabi::ethereum_types::U256;
 use ethabi::{ParamType, Token};
 use risc0_zkvm::guest::env;
@@ -34,12 +30,9 @@ fn fibonacci(n: U256) -> U256 {
 }
 
 pub fn main() {
-    // TODO(victor): This is uglier than it needs to be because env::read_slice does not support
-    // reading the whole input and I do not want to demonstrate code that only works for fixed
-    // sized values. An alternative would be to read a length, then read that number of bytes, this
-    // will depend on what the real watcher wants to do.
-    // Decode input passed from the application contract by the Bonsai bridge.
-    let input = ethabi::decode_whole(&[ParamType::Uint(256)], &env::read::<Vec<u8>>()).unwrap();
+    // NOTE: Currently env::read_slice requires a length argument. Reading `bytes` or another
+    // dynamically lengthed type will require some work. https://github.com/risc0/risc0/issues/402
+    let input = ethabi::decode_whole(&[ParamType::Uint(256)], env::read_slice(32)).unwrap();
     let n: U256 = input[0].clone().into_uint().unwrap();
 
     // Run the computation.

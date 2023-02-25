e54e78e144c67c266f309cb5cb2ed4337373c6e1
Victor Graf <victor@subgraf.dev>
2023-02-24 17:02:43 -0800

    add poke CLI command

--- cli/src/bin/poke.rs
+++ cli/src/bin/poke.rs
@@ -13,19 +13,18 @@
 // limitations under the License.
 
 use std::error::Error;
-use std::ops::Deref;
 use std::str::FromStr;
 use std::sync::Arc;
 
 use clap::Parser;
 use ethers::middleware::SignerMiddleware;
+use ethers::signers::Signer;
 use ethers::providers::{Http, Middleware, Provider};
-use ethers::signers::{LocalWallet, Signer};
-use ethers::types::{Address, H256};
+use ethers::signers::{LocalWallet};
+use ethers::types::{Address};
+use ethers::types::U256;
+use ethers::providers::StreamExt;
 use hello_bonsai_contracts::HelloBonsai;
-use hello_bonsai_methods::{FIBONACCI_ELF, FIBONACCI_ID};
-use reqwest::{Client, Url};
-use risc0_zkvm::sha::{self, Digest, Sha256};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -35,41 +34,14 @@ struct Args {
     #[clap(short = 'e', long, env, value_hint = clap::ValueHint::Url)]
     ethereum_node_url: String,
 
-    /// URL for the Bonsai service for to upload the ELF binary.
-    #[clap(short = 'b', long, env, value_hint = clap::ValueHint::Url)]
-    bonsai_url: String,
-
-    /// Ethereum contract address for the Bonsai proxy.
-    #[clap(short = 'p', long, env)]
-    bonsai_proxy_contract_address: Address,
-
-    /// API Key for Bonsai to authorize requests.
-    #[clap(env, long)]
-    bonsai_api_key: String,
+    /// HelloBonsai contract address.
+    #[clap(short = 'a', long, env)]
+    hello_bonsai_contract_address: Address,
 
     /// Ethereum private key to use for sending transactions.
     /// TODO(victor) Provide other ways to handle this.
     #[clap(env, long)]
     ethereum_private_key: String,
-}
-
-async fn upload_elf(
-    bonsai_url: &Url,
-    bonsai_api_key: &str,
-    elf: &'static [u8],
-) -> Result<String, Box<dyn Error>> {
-    let elf_hash = hex::encode(sha::Impl::hash_bytes(elf).deref());
-    Client::new()
-        .put(bonsai_url.join(&format!("/v1/elf/{elf_hash}"))?)
-        .bearer_auth(bonsai_api_key)
-        .header("Content-Type", "application/octet-stream")
-        .header("Expect", "100-continue")
-        .body(elf)
-        .send()
-        .await?
-        .error_for_status()?;
-
-    Ok(elf_hash)
 }
 
 #[tokio::main]
@@ -89,31 +61,33 @@ async fn main() -> Result<(), Box<dyn Error>> {
         wallet.with_chain_id(chain_id.as_u64()),
     ));
 
-    // Upload the guest binary to Bonsai.
-    println!("Uploading guest binary to Bonsai...");
-    let elf_hash = upload_elf(
-        &Url::parse(&args.bonsai_url)?,
-        &args.bonsai_api_key,
-        FIBONACCI_ELF,
-    )
-    .await?;
-    println!("Uploaded guest binary");
-    println!("    SHA-256:  {}", elf_hash);
-    println!("    Image ID: {}", hex::encode(Digest::from(FIBONACCI_ID)));
-
     // Deploy the HelloBonsai contract.
-    println!("Deploying guest binary to Bonsai...");
-    let hello_bonsai = HelloBonsai::deploy(
-        client.clone(),
-        (
-            args.bonsai_proxy_contract_address,
-            H256(Digest::from(FIBONACCI_ID).into()),
-        ),
-    )?
-    .send()
-    .await?;
-    println!("Deployed HelloBonsai contract at");
-    println!("    HelloBonsai address: {:?}", hello_bonsai.address());
+    let hello_bonsai = HelloBonsai::new(args.hello_bonsai_contract_address, client.clone());
+
+    // Subscribe to events on HelloBonsai.
+    let events = hello_bonsai.events();
+    let mut subscription = events.stream().await?;
+
+    // Call a function which offloads work to Bonsai.
+    println!("Sending transaction for HelloBonsai.calculate_fibonacci...");
+    let receipt = hello_bonsai
+        .calculate_fibonacci(U256::from(10))
+        .send()
+        .await?
+        .confirmations(1)
+        .await?;
+    println!("Transaction processed");
+    println!("    Hash: {:?}", receipt.ok_or("no receipt from transaction")?.transaction_hash);
+
+    // Wait for the callback to come from Bonsai.
+    println!("Waiting for callback event");
+    let callback_log = subscription.next().await.unwrap()?;
+    println!("    Log: {:?}", callback_log);
+
+    // Check that the expected changes took place on the contract.
+    println!("Calling HelloBonsai.fibonacci(10)");
+    let result: U256 = hello_bonsai.fibonacci(U256::from(10)).call().await?;
+    println!(" Result: {}", result);
 
     Ok(())
 }
